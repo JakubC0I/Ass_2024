8da5948784ef399c2d0ca68f84cc06c1d98514e9
JakubC0I <JakubC.01@protonmail.com>
2024-07-16 21:56:17 +0200

    add working no debug path

--- Ass_2024/Klausur2022.asm
+++ Ass_2024/Klausur2022.asm
@@ -13,29 +13,99 @@ includelib \masm32\lib\masm32.lib
 
 .data
 	inputVar db 1000 dup(?), 0
+	inputVarDebug db "|A|", 1500 dup(?), 0
 	debugger_msg db "debug modus anschalten? (j - ja / n - nein)",10, 0 ; 10 is a newline and 0 is string end
-	flag dd ?
-	result db 100 dup(?), 0 ; change it into array of ints
-	char_to_add db ?
+	words dd ?
+	words_result dd ?
+	characters_result dd ?
+
 .code
 
+; flag, #words, #characters
 handle_debug proc 
 	push ebp
 	mov ebp, esp
 
-	mov eax, [ebp + 8] ; eax contains the flag
+	mov eax, [ebp + 16] ; eax contains the flag
+	; xor esi, esi
+	
+	lea si, inputVar
+	mov ebx, esi
 
+retry_debug:
 	cmp al, 'j'
 	je ja_debug
 
 	cmp al, 'n'
 	je nein_debug
 
-conti:
+	jmp return_results
+
+nein_debug:
+	printf("Debugger mode deactivated\n")
+
+conti_no_debug:
+	xor al, al
+	lodsb
+
+	cmp al, ' ' ;compare to space to add it as a word
+	je add_word_no_debug
+
+	cmp al, 0 ;compare to end of a string
+	je return_results
+	
+	jmp conti_no_debug
+
+
+add_word_no_debug:
+	inc words
+
+	jmp conti_no_debug
+
+ja_debug:
+	printf("Debugger mode activated\n")
+
+conti_debug:
+	movsb
+
+	cmp al, ' ' ;compare to space to add it as a word
+	je add_word_no_debug
+
+	cmp al, 0 ;compare to end of a string
+	je return_results
+	
+	jmp conti_debug
+
+add_word_debug:
+	inc words
+	mov di, '|'
+	inc di
+	mov di, 'E'
+	inc di
+	mov di, '|'
+	inc di
+	mov di, ' '
+
+	jmp conti_debug
+
+return_results:
+	sub esi, ebx
+	dec esi ; starts with 1 and counts 0 at the end
+	dec esi
+
+	; TODO make it use pop instead
+	mov ecx, [words]
+	inc ecx ; add first word / number of spaces + 1
+	mov ebx, [ebp + 12] ;
+	mov [ebx], ecx ; number of words
+
+	inc esi
+	mov ebx, [ebp + 8]
+	mov [ebx], esi ; number of characters
 
 	mov esp, ebp
 	pop ebp
-	ret 4 ; 4 x #input_params
+	ret 12 ; 4 x #input_params
 handle_debug endp
 
 greet_and_take:
@@ -50,16 +120,10 @@ greet_and_take:
 	call StdIn
 	jmp after_greet
 
-ja_debug:
-	printf("Debugger modus activated\n")
-	jmp conti:
-
-nein_debug:
-	printf("Debugger modus deactivated\n")
-	jmp conti:
-
 klausur2022:
-	mov eax, 0
+	xor eax, eax
+	xor ebx, ebx
+
 	
 	jmp greet_and_take
 
@@ -68,12 +132,23 @@ after_greet:
 	call StdOut
 
 	getkey() ; takes one key input and saves it in al
+	
 	push eax
+	push offset words_result
+	push offset characters_result
+	
 	call handle_debug
+
+	cmp eax, 0
+	je goodbye
 
 	jmp after_greet
 
 goodbye:
+	mov eax, words_result
+	mov ebx, characters_result
+	printf("Number of characters: %d\nNumber of words: %d", ebx, eax)
+
 	xor eax, eax
 	exit
 
