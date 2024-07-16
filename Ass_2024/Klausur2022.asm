236672f586cac0bdd8d49fcabc6a9daf48f20165
JakubC0I <JakubC.01@protonmail.com>
2024-07-16 19:15:14 +0200

    create basic program structure

--- Ass_2024/Klausur2022.asm
+++ Ass_2024/Klausur2022.asm
@@ -12,29 +12,70 @@ includelib \masm32\lib\masm32.lib
 
 
 .data
-	inputVar db 100 dup(?), 0
-	first dd ?
-	second dd ?
+	inputVar db 1000 dup(?), 0
+	debugger_msg db "debug modus anschalten? (j - ja / n - nein)",10, 0 ; 10 is a newline and 0 is string end
+	flag dd ?
 	result db 100 dup(?), 0 ; change it into array of ints
 	char_to_add db ?
 .code
+
+handle_debug proc 
+	push ebp
+	mov ebp, esp
+
+	mov eax, [ebp + 8] ; eax contains the flag
+
+	cmp al, 'j'
+	je ja_debug
+
+	cmp al, 'n'
+	je nein_debug
+
+conti:
+
+	mov esp, ebp
+	pop ebp
+	ret 4 ; 4 x #input_params
+handle_debug endp
 
 greet_and_take:
 	printf("Jakub Ciszak, 0112228\n")
 	printf("Enter some text: ")
 	
 	; Define string size
-	push 100
+	push 1000
 
 	; Define where to put it (address)
 	push offset inputVar
 	call StdIn
+	jmp after_greet
+
+ja_debug:
+	printf("Debugger modus activated\n")
+	jmp conti:
+
+nein_debug:
+	printf("Debugger modus deactivated\n")
+	jmp conti:
 
 klausur2022:
 	mov eax, 0
-	mov ebx, 0
-	mov edx, 0
 	
 	jmp greet_and_take
+
+after_greet:
+	push offset debugger_msg
+	call StdOut
+
+	getkey() ; takes one key input and saves it in al
+	push eax
+	call handle_debug
+
+	jmp after_greet
+
+goodbye:
+	xor eax, eax
+	exit
+
 
 end klausur2022
---
+++ Ass_2024/run.bat
@@ -0,0 +1,3 @@
+C:\masm32\bin\ml /c /coff Klausur2022.asm
+C:\masm32\bin\polink.exe /SUBSYSTEM:CONSOLE Klausur2022.obj
+Klausur2022.exe
