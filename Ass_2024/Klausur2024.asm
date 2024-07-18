50f0125ac0ed1bf664eb81ca7b233a29ea41968d
JakubC <jc87puqu@stud.hszg.de>
2024-07-18 13:34:37 +0200

    exam

--- Ass_2024/Klausur2024.asm
+++ Ass_2024/Klausur2024.asm
@@ -15,29 +15,91 @@ includelib \masm32\lib\masm32.lib
 	inputVar db 1000 dup(?), 0
 	debugger_msg db "debug modus anschalten? (j - ja / n - nein)",10, 0 ; 10 is a newline and 0 is string end
 	flag dd ?
-	result db 100 dup(?), 0 ; change it into array of ints
+	result_year db ? ; as number
+	result_year_string db 4 dup(?), 0
 	char_to_add db ?
 .code
+
+check_if_number macro value
+	; required to declare which labels are local to a macro
+	LOCAL stufe_zwei, is_a_number, skip, goodbye_macro
+
+		cmp value, 48
+		jge stufe_zwei
+
+		jmp skip
+
+		stufe_zwei:
+			cmp value, 57
+			jle is_a_number
+skip:
+	mov ecx, 0
+	jmp goodbye_macro
+
+is_a_number:
+	; from string to number
+	; sub value, 48
+	mov ecx, value
+goodbye_macro:
+	exitm
+	endm
 
 handle proc ;use parameters
 	push ebp
 	mov ebp, esp
 
-	lea si, inputVar
+	mov eax, [ebp + 12]
+
+	lea si, [eax]
+	xor eax, eax
 	scan_string:
 		lodsb ; load first byte from an input string
 
 		cmp al, 0
 		je return_and_bye
 
-		printf("%c", al)
+		jmp first_n
 
 		jmp scan_string
+		
 
 	return_and_bye:
 		mov esp, ebp
 		pop ebp
-		ret 4 ; 4 x #input_params
+		ret 8 ; 4 x #input_params
+
+
+	first_n:
+		check_if_number(eax)
+		cmp ecx, 0
+		je scan_string
+		movsb
+
+	second_n:
+		mov al, [esi]
+		check_if_number(eax)
+		cmp ecx, 0
+		je scan_string
+
+	third_n:
+		mov al, [esi]
+		check_if_number(eax)
+		cmp ecx, 0
+		je scan_string
+		movsb
+
+	fourth_n:
+		mov al, [esi]
+		check_if_number(eax)
+		cmp ecx, 0
+		je scan_string
+
+	fivth_not_a_number:
+		mov al, [esi]
+		check_if_number(eax)
+		cmp ecx, 0
+		je return_and_bye
+		jmp scan_string
 
 handle endp
 
@@ -56,16 +118,15 @@ greet_and_take:
 
 klausur2024:
 	mov eax, 0
+	mov bx, 10
+	lea di, result_year_string
 	
 	jmp greet_and_take
 
 after_greet:
-;#####################JUST CHANGE WELECOMING MESSAGE
-	push offset debugger_msg
-	call StdOut
-;#####################GET 1 KEY
-	getkey() ; takes one key input and saves it in al
-	push eax
+
+	push offset inputVar
+	push offset result_year
 	call handle
 	
 	cmp al, 0
@@ -74,7 +135,12 @@ after_greet:
 	jmp after_greet
 
 goodbye:
-	xor eax, eax
+	inc di
+	mov [edi], eax
+
+	push offset result_year_string
+	call StdOut
+	; printf("%s", result_year_string)
 	exit
 
 
